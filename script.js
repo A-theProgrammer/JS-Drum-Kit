a0a2654f65f799eeb74635b91c92556a60322ece
Archit <architbiswas885@yahoo.com>
2022-02-19 02:49:56 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const playingSound=(e)=>{
+  const audio=document.querySelector(`audio[data-key="${e.key.charCodeAt()}"]`);
+  const key=document.querySelector(`.key[data-key="${e.key.charCodeAt()}"]`);
+  if(!audio){
+    return
+  };
+  audio.currentTime=0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+const removeTransition=(e)=>{
+  if (e.propertyName!=="transform"){
+    return;
+  }
+  e.target.classList.remove("playing");
+};
+
+const keys=document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend",removeTransition));
+
+window.addEventListener('keydown',playingSound);
